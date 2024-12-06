d2258202255a72eeb4fdeeb665447fa7eb5ef68f
slv <ctpo-1@l1523.ru>
2024-12-06 17:54:10 +0300

    new_feature

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ a = 34
 
 if __name__ == "__main__":
     print('Hello, world!')
+    print('Another words')
     while True:
         break
---
+++ new_feature1.py
@@ -0,0 +1,5 @@
+while True:
+    a = input()
+    if a == 'S|TOP':
+        break
+    print(a)
